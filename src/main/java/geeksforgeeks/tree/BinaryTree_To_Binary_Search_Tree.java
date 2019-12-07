3ff98b37b6b697ea3613ee0986998216b202bd15
Devdutt <devduttjadeja12345@gmail.com>
2019-12-06 22:21:37 -0500

    chnages in class and added new class

---
+++ src/main/java/geeksforgeeks/tree/BinaryTree_To_Binary_Search_Tree.java
@@ -0,0 +1,12 @@
+package geeksforgeeks.tree;
+
+// https://www.geeksforgeeks.org/binary-tree-to-binary-search-tree-conversion/
+
+public class BinaryTree_To_Binary_Search_Tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
