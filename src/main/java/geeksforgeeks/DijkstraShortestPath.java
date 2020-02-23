a1a43b0296939d7c575b21bf6b43f0fb159b49e2
Devdutt <devduttjadeja12345@gmail.com>
2020-02-22 21:50:15 -0500

    Added 2 new classes

---
+++ src/main/java/geeksforgeeks/DijkstraShortestPath.java
@@ -0,0 +1,8 @@
+package geeksforgeeks;
+
+public class DijkstraShortestPath {
+
+// https://www.youtube.com/watch?v=pVfj6mxhdMw&list=TLPQMjMwMjIwMjCp3SNzeKq0Iw&index=5
+
+
+}
---
+++ src/main/java/geeksforgeeks/PriorityQueueDemo.java
@@ -0,0 +1,30 @@
+package geeksforgeeks;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+    public static void main(String args[]) {
+
+        // Creating empty priority queue
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        // Adding items to the pQueue using add()
+        pq.add(100);
+        pq.add(20);
+        pq.add(15);
+
+        // Printing the top element of PriorityQueue
+        System.out.println(pq.peek());
+
+        // Printing the top element and removing it
+        // from the PriorityQueue container
+        System.out.println(pq.poll());
+
+        // Printing the top element again
+        System.out.println(pq.peek());
+
+    }
+
+}
